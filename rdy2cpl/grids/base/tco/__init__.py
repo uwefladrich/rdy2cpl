2a6e1283011dc18e502edbb2276f37a658592535
oloapinivad <p.davini@isac.cnr.it>
2024-02-06 16:38:55 +0000

    tco399

--- rdy2cpl/grids/base/tco/__init__.py
+++ rdy2cpl/grids/base/tco/__init__.py
@@ -1,3 +1,4 @@
 from .tco95 import Tco95
 from .tco159 import Tco159
 from .tco319 import Tco319
+from .tco399 import Tco399
--- rdy2cpl/grids/couple_grid.py
+++ rdy2cpl/grids/couple_grid.py
@@ -6,7 +6,7 @@ import yaml
 from rdy2cpl.grids.base.orca import OrcaTGrid, OrcaUGrid, OrcaVGrid
 from rdy2cpl.grids.base.regular import EquidistantLatLonGrid
 from rdy2cpl.grids.base.regular_gaussian import N128
-from rdy2cpl.grids.base.tco import Tco95, Tco159, Tco319
+from rdy2cpl.grids.base.tco import Tco95, Tco159, Tco319, Tco399
 from rdy2cpl.grids.base.tl import Tl159
 from rdy2cpl.grids.mask_modifiers import (
     invert_mask,
@@ -32,6 +32,7 @@ _base_grids = {
         Tco95,
         Tco159,
         Tco319,
+        Tco399,
     )
 }
 
