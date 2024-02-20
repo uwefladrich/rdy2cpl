de508f66934eb01a1d5e0c3f5e4f5452d2f57aed
oloapinivad <p.davini@isac.cnr.it>
2024-02-20 11:06:10 +0000

    tl255

--- rdy2cpl/grids/couple_grid.py
+++ rdy2cpl/grids/couple_grid.py
@@ -7,7 +7,7 @@ from rdy2cpl.grids.base.orca import OrcaTGrid, OrcaUGrid, OrcaVGrid
 from rdy2cpl.grids.base.regular import EquidistantLatLonGrid
 from rdy2cpl.grids.base.regular_gaussian import N128
 from rdy2cpl.grids.base.tco import Tco95, Tco159, Tco319, Tco399
-from rdy2cpl.grids.base.tl import Tl63, Tl159
+from rdy2cpl.grids.base.tl import Tl63, Tl159, Tl255
 from rdy2cpl.grids.mask_modifiers import (
     invert_mask,
     mask_box,
@@ -30,6 +30,7 @@ _base_grids = {
         OrcaVGrid,
         Tl63,
         Tl159,
+        Tl255,
         Tco95,
         Tco159,
         Tco319,
